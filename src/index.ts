01c6a900a5c705971f86004ae66177fbd9feae77
Yusin <sunkatabeckama@gmail.com>
2024-10-26 13:15:29 +0200

    Updates

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,6 @@ app.use("/teams", teamRoutes);
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, () => {
   console.log(`Server running on part ${port}`);
 });
